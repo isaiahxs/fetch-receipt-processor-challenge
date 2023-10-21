3286940e3d568d911f67f354292397429eedf5d3
isaiah <isaiahsinnathamby@gmail.com>
2023-10-21 10:58:59 -0400

    added functions and tests for ItemsCount + ItemDescriptions + pointsForDay

--- backend/points.js
+++ backend/points.js
@@ -17,8 +17,35 @@ const pointsForRoundTotal = (total) => parseFloat(total) % 1 === 0 ? 50 : 0;
 // 25 points if the total is a multiple of 0.25
 const pointsForMultipleOfQuarter = (total) => parseFloat(total) % 0.25 === 0 ? 25 : 0;
 
+// 5 points for every two items on the receipt. Using Math.floor to ensure that only complete pairs contribute to the score.
+const pointsForItemsCount = (items) => Math.floor(items.length / 2) * 5;
+
+const pointsForItemDescriptions = (items) => {
+    let points = 0;
+    for (let item of items) {
+        // If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer
+        const descriptionLength = item.shortDescription.trim().length;
+
+        if (descriptionLength % 3 === 0) {
+            points += Math.ceil(parseFloat(item.price) * 0.2);
+        }
+    }
+    // The result is the number of points earned
+    return points;
+};
+
+// 6 points if the day in the purchase date is odd
+const pointsForDay = (purchaseDate) => {
+    const day = parseInt(purchaseDate.split('-')[2]);
+    return day % 2 === 1 ? 6 : 0;
+}
+
+
 module.exports.router = router;
 module.exports.receipts = receipts;
 module.exports.pointsForRetailerName = pointsForRetailerName;
 module.exports.pointsForRoundTotal = pointsForRoundTotal;
 module.exports.pointsForMultipleOfQuarter = pointsForMultipleOfQuarter;
+module.exports.pointsForItemsCount = pointsForItemsCount;
+module.exports.pointsForItemDescriptions = pointsForItemDescriptions;
+module.exports.pointsForDay = pointsForDay;
--- backend/points.test.js
+++ backend/points.test.js
@@ -5,7 +5,10 @@ const { receipts } = require('./points');
 const {
     pointsForRetailerName,
     pointsForRoundTotal,
-    pointsForMultipleOfQuarter
+    pointsForMultipleOfQuarter,
+    pointsForItemsCount,
+    pointsForItemDescriptions,
+    pointsForDay,
 } = require('./points');
 const { default: expect } = require('expect');
 
@@ -25,4 +28,59 @@ describe('Points calculation functions', () => {
         expect(pointsForMultipleOfQuarter('1.25')).toBe(25);
         expect(pointsForMultipleOfQuarter('1.01')).toBe(0);
     });
+
+    test('calculates points for every two items', () => {
+        expect(pointsForItemsCount([
+            { shortDescription: 'Pepsi - 12-0z', price: '1.25' },
+            { shortDescription: 'Dasani', price: '1.40' }
+        ])).toBe(5);
+
+        expect(pointsForItemsCount([
+            { shortDescription: 'Pepsi - 12-0z', price: '1.25' },
+        ])).toBe(0);
+
+        expect(pointsForItemsCount([
+            { shortDescription: 'Pepsi - 12-0z', price: '1.25' },
+            { shortDescription: 'Dasani', price: '1.40' },
+            { shortDescription: 'Pepsi - 12-0z', price: '1.25' },
+            { shortDescription: 'Dasani', price: '1.40' }
+        ])).toBe(10);
+    })
+
+    test('calculates points for item descriptions', () => {
+        let items = [
+            { shortDescription: 'Dasani', price: '1.40' }, // 6 characters, 1 point
+            { shortDescription: 'Pepsi', price: '1.25' } // 5 characters, 0 points
+        ]
+        expect(pointsForItemDescriptions(items)).toBe(1);
+
+        items = [
+            { shortDescription: 'Water', price: '1.00' }, // 5 characters, 0 points
+            { shortDescription: 'Tea', price: '1.25' } // 3 characters, 1 point
+        ]
+        expect(pointsForItemDescriptions(items)).toBe(1);
+
+        items = [
+            { shortDescription: 'Water', price: '1.00' }, // 5 characters, 0 points
+            { shortDescription: 'Teas', price: '1.35' } // 4 characters, 0 points
+        ]
+        expect(pointsForItemDescriptions(items)).toBe(0);
+
+        items = []
+        expect(pointsForItemDescriptions(items)).toBe(0);
+    })
+
+    test('calculates points based on day', () => {
+        expect(pointsForDay('2022-01-01')).toBe(6);
+        expect(pointsForDay('2022-01-03')).toBe(6);
+        expect(pointsForDay('2022-01-05')).toBe(6);
+
+        expect(pointsForDay('2022-01-02')).toBe(0);
+        expect(pointsForDay('2022-01-04')).toBe(0);
+        expect(pointsForDay('2022-01-06')).toBe(0);
+
+        expect(pointsForDay('2025-12-01')).toBe(6);
+        expect(pointsForDay('2025-12-02')).toBe(0);
+    })
+
 })
