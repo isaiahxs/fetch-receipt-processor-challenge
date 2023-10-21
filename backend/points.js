09060189d4b047cf4c62722e3f3efada3e63ede8
isaiah <isaiahsinnathamby@gmail.com>
2023-10-21 10:36:20 -0400

    passing tests for RetailerName + RoundTotal + MultipleOfQuarter

--- backend/points.js
+++ backend/points.js
@@ -11,6 +11,14 @@ const receipts = {};
 // One point for every alphanumeric character in the retailer name
 const pointsForRetailerName = (retailer) => retailer.replace(/[^a-zA-Z0-9]/g, '').length;
 
+// 50 points if the total is a round dollar amound with no cents
+const pointsForRoundTotal = (total) => parseFloat(total) % 1 === 0 ? 50 : 0;
+
+// 25 points if the total is a multiple of 0.25
+const pointsForMultipleOfQuarter = (total) => parseFloat(total) % 0.25 === 0 ? 25 : 0;
+
 module.exports.router = router;
 module.exports.receipts = receipts;
 module.exports.pointsForRetailerName = pointsForRetailerName;
+module.exports.pointsForRoundTotal = pointsForRoundTotal;
+module.exports.pointsForMultipleOfQuarter = pointsForMultipleOfQuarter;
--- backend/points.test.js
+++ backend/points.test.js
@@ -4,11 +4,25 @@ const { receipts } = require('./points');
 
 const {
     pointsForRetailerName,
+    pointsForRoundTotal,
+    pointsForMultipleOfQuarter
 } = require('./points');
+const { default: expect } = require('expect');
 
 describe('Points calculation functions', () => {
     test('calculates points for retailer name', () => {
         expect(pointsForRetailerName('Target')).toBe(6);
         expect(pointsForRetailerName('Walmart')).toBe(7);
-    })
+    });
+
+    test('calculates points for rounded total', () => {
+        expect(pointsForRoundTotal('1.00')).toBe(50);
+        expect(pointsForRoundTotal('1.01')).toBe(0);
+    });
+
+    test('calculates multiple points for multiples of 0.25', () => {
+        expect(pointsForMultipleOfQuarter('1.50')).toBe(25);
+        expect(pointsForMultipleOfQuarter('1.25')).toBe(25);
+        expect(pointsForMultipleOfQuarter('1.01')).toBe(0);
+    });
 })
