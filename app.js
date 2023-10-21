37c73c5ac711706d45a9242f7718ed2cf7d54e5c
isaiah <isaiahsinnathamby@gmail.com>
2023-10-21 10:14:30 -0400

    added backend and frontend directories + ran npm init -y - will start on points logic and functions soon

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { router: pointsRouter } = require('./backend/routes/api/points');
+const path = require('path');
+const app = express();
+const port = 3001;
+
+// Our middleware for parsing JSON bodies
+app.use(express.json());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, './frontend/build')));
+
+// API routes
+app.use('/', pointsRouter);
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
+
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
+
+module.exports = app;
