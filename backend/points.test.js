5078d6dbc8ad34d793ad8a1651820fd311d00b64
isaiah <isaiahsinnathamby@gmail.com>
2023-10-21 11:27:52 -0400

    added test for receipts/process route

--- backend/points.test.js
+++ backend/points.test.js
@@ -123,3 +123,80 @@ describe('Points calculation functions', () => {
         expect(calculatePoints(receipt)).toBe(31);
     })
 })
+
+// Testing API endpoints
+describe('POST /receipts/process', () => {
+    it('should return 400 if retailer is not provided', async () => {
+        const res = await request(app)
+            .post('/receipts/process')
+            .send({
+                retailer: '',
+                purchaseDate: '2022-12-31',
+                purchaseTime: '12:34',
+                total: '123.45',
+                items: [
+                    { shortDescription: 'item1', price: '1.23' },
+                    { shortDescription: 'item2', price: '4.56' }
+                ]
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('errors');
+    });
+
+    it('should return 400 for invalid purchase date', async () => {
+        const res = await request(app)
+            .post('/receipts/process')
+            .send({ retailer: 'Walmart', purchaseDate: 'invalid-date' });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('errors');
+    });
+
+    it('should return 400 for invalid purchase time', async () => {
+        const res = await request(app)
+            .post('/receipts/process')
+            .send({ retailer: 'Walmart', purchaseDate: '2023-01-01', purchaseTime: 'invalid-time' });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.errors).toContain('Invalid purchase time. Structure must be HH:MM.');
+    });
+
+    it('should return 400 for invalid total', async () => {
+        const res = await request(app)
+            .post('/receipts/process')
+            .send({ retailer: 'Walmart', purchaseDate: '2023-01-01', purchaseTime: '12:34', total: 'invalid-total' });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.errors).toContain('Invalid total. Must be a valid number.');
+    });
+
+    it('should return 400 for invalid items', async () => {
+        const res = await request(app)
+            .post('/receipts/process')
+            .send({
+                retailer: 'Walmart',
+                purchaseDate: '2023-01-01',
+                purchaseTime: '12:34',
+                total: '123.45'
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.errors).toContain('Invalid items. Must be a non-empty array.');
+    });
+
+    it('should successfully process a valid receipt', async () => {
+        const validReceipt = {
+            retailer: 'Walmart',
+            purchaseDate: '2023-10-11',
+            purchaseTime: '14:00',
+            total: 100.0,
+            items: [
+                { shortDescription: 'item1', price: 50 },
+                { shortDescription: 'item2', price: 50 },
+            ],
+        };
+
+        const response = await request(app)
+            .post('/receipts/process')
+            .send(validReceipt);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('id');
+    });
+});
