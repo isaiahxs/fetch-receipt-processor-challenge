fd4b6fa269d6d3b8711c53b8999fbb0b07f29d39
isaiah <isaiahsinnathamby@gmail.com>
2023-10-28 15:30:01 -0400

    organized backend files into backend directory

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const port = 3001;
-
-const server = app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
-
-module.exports = server;
