89358baf525c2d8006af5cfc54e4126445a0e35e
isaiah <isaiahsinnathamby@gmail.com>
2023-10-28 15:12:32 -0400

    removed unnecessary files

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import { useState, useReducer } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../HomePage';
+import NotFound from '../NotFound';
 
 export default function Main() {
     return (
         <main>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </main>
     )
